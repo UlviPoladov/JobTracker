90732f0e8fa634ff84989db7c9cda5444f1b9b81
Ulvi <ulvi.poladov207@gmail.com>
2026-04-30 12:12:50 +0300

    add auto migrate

--- backend/JobTracker.API/Program.cs
+++ backend/JobTracker.API/Program.cs
@@ -72,6 +72,12 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 
 
 app.Run();
